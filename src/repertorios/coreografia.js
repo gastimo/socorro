f47fc43a6e4f229b3e840130ce60d007bc903ef1
Gastón Martino <gaston.martino@gmail.com>
2026-03-07 21:03:11 -0300

    Se añade la coreografía "CONCURRENTE"

--- src/repertorios/coreografia.js
+++ src/repertorios/coreografia.js
@@ -58,7 +58,7 @@ const Coreografia = (S) => {
     const angulo = (puestos <= 1 ? 0 : 2 * Math.PI / puestos * actor.puesto) + desvio;
     
     // A partir del ángulo (dirección), se calcula el vector de separación
-    const corrimiento = S.O.S.Vector(S.O.S.escalar(separacion), 0, 0);
+    const corrimiento = S.O.S.Vector(separacion, 0, 0);
     corrimiento.ang(angulo);
     
     // Luego, con el mismo ángulo (o dirección), se define el vector de velocidad
@@ -84,6 +84,25 @@ const Coreografia = (S) => {
 //
 // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 _COREOS[CONCURRENTE] = (actor, cantidad, puestos, intensidad, desvio, separacion) => {
+    // Se calcula el ángulo en función de la cantidad de puestos
+    const angulo = (puestos <= 1 ? 0 : 2 * Math.PI / puestos * actor.puesto) + desvio;
+    
+    // A partir del ángulo (dirección), se calcula el vector de separación
+    const distancia = Math.min(S.O.S.ancho(), S.O.S.alto()) / 2;
+    const corrimiento = S.O.S.Vector(distancia + separacion, 0, 0);
+    corrimiento.ang(angulo);
+    
+    // Luego, con el mismo ángulo (o dirección), se define el vector de velocidad
+    const velocidad = S.O.S.Vector(intensidad, 0, 0);
+    velocidad.ang(angulo);
+    
+    // Finalmente, a la posición origen (centro del lienzo) se le suma la separación
+    const origen = S.O.S.Vector(0, 0, 0);
+    origen.sumar(corrimiento);
+    
+    // Se actualiza el origen y la velocidad del "Actor" recibido como argumento
+    actor.defOrigen(origen);
+    actor.defVelocidad(velocidad.multiplicar(-1));
 };
     
     
